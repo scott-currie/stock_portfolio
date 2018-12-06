e1a47d25bfd1df4ae7e5037ded8e733694a9fa18
Scott Currie <vitriolik@gmail.com>
2018-12-06 15:18:58 -0800

    Fixed the init stuff for starting the app.

--- src/__init__.py
+++ src/__init__.py
@@ -1,22 +1,22 @@
-###########
-# IMPORTS #
-###########
-# Generic Flask Requirements
-from . import routes, forms, models, exceptions
 from flask import Flask
 import os
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-######################
-# APPLICATION SETUPS #
-######################
-# `flask run` - runs application on local server
-app = Flask(__name__, static_url_path='', static_folder='static',
-            instance_relative_config=True)
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='static',
+    instance_relative_config=True
+)
 
 app.config.from_mapping(
     SECRET_KEY=os.getenv('SECRET_KEY'),
     SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL'),
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
+
+
+from . import routes, forms, models, exceptions
