f72be10fea0ab9d8bac316ed1d0bd504d742382c
Scott Currie <vitriolik@gmail.com>
2018-12-11 08:59:38 -0800

    Added some tests.

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -3,6 +3,13 @@ import pytest
 
 
 def test_home_route_get():
-    rv = app.test_client.get('/')
-    import pydb; pydb.start_trace()
+    rv = app.test_client().get('/')
     assert rv.status_code == 200
+
+def test_portfolio_route_get():
+    rv = app.test_client().get('/search')
+    assert rv.status_code == 200
+
+def test_portfolio_route_post():
+    rv = app.test_client().post('/search', data={'symbol': 'MSFT'})
+    assert rv.status_code == 302
