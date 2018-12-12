9e7eb53cad84ff9875c0053966bc38dbc76ba515
Scott Currie <vitriolik@gmail.com>
2018-12-12 14:32:41 -0800

    Added Company model

--- src/models.py
+++ src/models.py
@@ -12,6 +12,7 @@ class Company(db.Model):
     __tablename__ = 'companies'
 
     id = db.Column(db.Integer, primary_key=True)
+    portfolio_id = db.Column(db.ForeignKey('portfolios.id'), nullable=False)
     symbol = db.Column(db.String(64), index=True, unique=True)
     companyName = db.Column(db.String(256), index=True, unique=True)
     exchange = db.Column(db.String(128))
@@ -26,3 +27,15 @@ class Company(db.Model):
 
     def __repr__(self):
         return '<Company {}>'.format(self.companyName)
+
+
+class Portfolio(db.Model):
+    __tablename__ = 'portfolios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), index=True)
+    companies = db.relationship('Company', backref='portfolio', lazy=True)
+    date_created = db.Column(db.DateTime, default=dt.now())
+
+    def __repr__(self):
+        return '<Portfolio {}>'.format(self.name)
