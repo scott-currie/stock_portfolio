5afa79536efd7eb5952f8fd4ab283c64f4ec11a5
Scott Currie <vitriolik@gmail.com>
2018-12-11 19:56:21 -0800

    Added imports. Refactored company_preview.

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,12 @@
-from flask import render_template, abort, redirect, url_for, request
+from flask import render_template, abort, redirect, url_for, request, session, flash
 from json import JSONDecodeError
 from .models import db, Company
 import requests as req
 from . import app
-from .forms import CompanySearchForm
+from .forms import CompanySearchForm, CompanyAddForm
 import json
 import requests
+from sqlalchemy.exc import DBAPIError, IntegrityError
 
 
 @app.route('/')
@@ -21,38 +22,59 @@ def company_search():
     """
     form = CompanySearchForm()
 
+    # This path represents a POST
     if form.validate_on_submit():
         symbol = form.data['symbol']
         res = requests.get(f'https://api.iextrading.com/1.0/stock/{ symbol }/company')
 
-
         data = json.loads(res.text)
+        session['context'] = data
 
-
-        return redirect(url_for('.portfolio_detail'))
-
+        return redirect(url_for('.company_preview'))
+    # This is a GET
     return render_template('portfolio/search.html', form=form)
 
 
 @app.route('/preview', methods=['GET', 'POST'])
-def preview_company():
+def company_preview():
     """
     """
-
-        # company = Company(
-        #     symbol=data['symbol'],
-        #     companyName=data['companyName'],
-        #     exchange=data['exchange'],
-        #     industry=data['industry'],
-        #     website=data['website'],
-        #     description=data['description'],
-        #     CEO=data['CEO'],
-        #     issueType=data['issueType'],
-        #     sector=data['sector'],
-        # )
-
-        # db.session.add(company)
-        # db.session.commit()
+    form_context = {
+        'symbol': session['context']['symbol'],
+        'companyName': session['context']['companyName'],
+        'exchange': session['context']['exchange'],
+        'industry': session['context']['industry'],
+        'website': session['context']['website'],
+        'description': session['context']['description'],
+        'CEO': session['context']['CEO'],
+        'issueType': session['context']['issueType'],
+        'sector': session['context']['sector']
+    }
+    form = CompanyAddForm(**form_context)
+    if form.validate_on_submit():
+        try:
+            company = Company(
+                symbol=form.data['symbol'],
+                companyName=form.data['companyName'],
+                exchange=form.data['exchange'],
+                industry=form.data['industry'],
+                website=form.data['website'],
+                description=form.data['description'],
+                CEO=form.data['CEO'],
+                issueType=form.data['issueType'],
+                sector=form.data['sector'],
+            )
+            db.session.add(company)
+            db.session.commit()
+        except (DBAPIError, IntegrityError):
+            flash('An error occurred trying to add this company.')
+            # Error in writing to db. End this req/res cycle and render search page.
+            return render_template('portfolio/search.html', form=form)
+        # Write was successful. Redirect to portfolio detail page
+        print('Successful write to database.')
+        return redirect(url_for('.portfolio_detail'))
+    # This was a POST method. Render the portfolio preview with form context
+    return render_template('portfolio/preview.html', form=form, company_data=session['context'])
 
 
 @app.route('/portfolio')
