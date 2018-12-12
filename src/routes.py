4298626929d560af62b58965e5fea5e4221b3b0d
Scott Currie <vitriolik@gmail.com>
2018-12-11 18:31:09 -0800

    Added imports. Refactoring routes.

--- src/routes.py
+++ src/routes.py
@@ -3,7 +3,9 @@ from json import JSONDecodeError
 from .models import db, Company
 import requests as req
 from . import app
+from .forms import CompanySearchForm
 import json
+import requests
 
 
 @app.route('/')
@@ -17,37 +19,45 @@ def home():
 def company_search():
     """
     """
-    if request.method == 'POST':
-        res = req.get(f'https://api.iextrading.com/1.0/stock/{ request.form.get("symbol") }/company')
+    form = CompanySearchForm()
 
-        try:
-            data = json.loads(res.text)
-            company = Company(
-                symbol=data['symbol'],
-                companyName=data['companyName'],
-                exchange=data['exchange'],
-                industry=data['industry'],
-                website=data['website'],
-                description=data['description'],
-                CEO=data['CEO'],
-                issueType=data['issueType'],
-                sector=data['sector'],
-            )
+    if form.validate_on_submit():
+        symbol = form.data['symbol']
+        res = requests.get(f'https://api.iextrading.com/1.0/stock/{ symbol }/company')
 
-            # NOTE: THIS WILL THROW A DUPE KEY ERROR IF WE ADD THE SAME STOCK AGAIN
-            # Handle this with an additional try/except
-            db.session.add(company)
-            db.session.commit()
 
-            return redirect(url_for('.portfolio_detail'))
-        except JSONDecodeError:
-            abort(404)
+        data = json.loads(res.text)
 
-    return render_template('portfolio/search.html')
+
+        return redirect(url_for('.portfolio_detail'))
+
+    return render_template('portfolio/search.html', form=form)
+
+
+@app.route('/preview', methods=['GET', 'POST'])
+def preview_company():
+    """
+    """
+
+        # company = Company(
+        #     symbol=data['symbol'],
+        #     companyName=data['companyName'],
+        #     exchange=data['exchange'],
+        #     industry=data['industry'],
+        #     website=data['website'],
+        #     description=data['description'],
+        #     CEO=data['CEO'],
+        #     issueType=data['issueType'],
+        #     sector=data['sector'],
+        # )
+
+        # db.session.add(company)
+        # db.session.commit()
 
 
 @app.route('/portfolio')
 def portfolio_detail():
     """
     """
-    return render_template('portfolio/portfolio.html')
+    companies = Company.query.all()
+    return render_template('portfolio/portfolio.html', companies=companies)
